35cde18e0eff0db9d1843aa446b7649e61fd098d
Haki-Malai <p17mala@ionio.gr>
2021-04-15 21:00:26 +0300

    For Testing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.body.classList.add("dark");
+  document.body.classList.add("dark")
 }
 window.onload = (event) => {
   var elements = document.getElementsByClassName('level-bar-inner')
-  console.log(config)
   for (let i = 0; i < elements.length; i++) {
     elements[i].style.width = '300px'
+    console.log(elements[i].dataset())
   }
 }
