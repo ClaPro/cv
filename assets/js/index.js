e31bfdfe3f3d1b6675f62774b7af89c10bdca7bd
Haki-Malai <p17mala@ionio.gr>
2021-04-15 20:21:57 +0300

    For Testing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
+const { config } = require("node:process");
+
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.body.classList.add("dark");
 }
 window.onload = (event) => {
   var elements = document.getElementsByClassName('level-bar-inner')
-  console.log(elements)
+  console.log(config)
   for (let i = 0; i < elements.length; i++) {
     elements[i].style.width = '300px'
   }
