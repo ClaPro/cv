77a858b8e02df69d4d52baac3974f169ef3b2500
Haki-Malai <p17mala@ionio.gr>
2021-04-15 18:03:28 +0300

    added some js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.body.classList.add("dark");
 }
+window.addEventListener('onload', ()=>{
+  var elements = document.getElementsByClassName('.level-bar-inner').width
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].width = '150'
+  }
+})
