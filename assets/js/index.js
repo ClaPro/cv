e12049ab6cc337dfaa8e85c623852e565a702800
Haki-Malai <p17mala@ionio.gr>
2021-04-15 19:13:20 +0300

    test

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.body.classList.add("dark");
 }
+
 window.addEventListener('onload', ()=>{
+  console.log('1')
   var elements = document.getElementsByClassName('.level-bar-inner').width
   for (let i = 0; i < elements.length; i++) {
     elements[i].style.width = '300 px'
