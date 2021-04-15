2bbf8e52d01633dc6bf7fe2d33ce98661961679d
Haki-Malai <p17mala@ionio.gr>
2021-04-15 19:31:40 +0300

    Minor

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,10 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.body.classList.add("dark");
 }
-console.log('1')
-var elements = document.getElementsByClassName('.level-bar-inner').width
-for (let i = 0; i < elements.length; i++) {
-  elements[i].style.width = '300 px'
+window.onload = (event) => {
+  console.log('1')
+  var elements = document.getElementsByClassName('.level-bar-inner').width
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.width = '300 px'
+  }
 }
